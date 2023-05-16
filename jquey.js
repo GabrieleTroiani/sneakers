cbaeda97d12e9da529e2754a741fa5fac8401bd2
GabrieleTroiani <133761236+GabrieleTroiani@users.noreply.github.com>
2023-05-16 12:09:35 +0200

    Add files via upload

---
+++ jquey.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $("#btn").click(function() {
+        alert("Acquisto avvenuto con successo");
+    });
+});
+
+$(document).ready(function() {
+    $("#login").click(function() {
+        alert("Grazie per aver fatto l'accesso al nostro sito");
+    });
+});
+
+$(document).ready(function() {
+    $("#access").click(function() {
+        alert("Hai fatto accesso al sito");
+    });
+});
